e58287bfcf300f63346c4ee001c4c001216cc3f8
Naveenavignesh <naveenanavii4437@gmail.com>
2024-05-21 06:12:13 -0700

    Add

---
+++ css/README.md
@@ -0,0 +1,3 @@
+How to compare two JSON have the same properties without order?
+ a. let obj1 ={ name: "person1",age5};
+ b. let obj2 -{ age:5,name: "person1"};
--- js/script.js
+++ js/script.js
@@ -2,11 +2,10 @@
 console.log("naveena")
 console.log("Person1")
 console.log("age:5")
-//}
+
 
 
 //let obj2= {age:5 "person1"  name}
 console.log("age:5")
 console.log("Person1")
 console.log("Priya")
-//}
