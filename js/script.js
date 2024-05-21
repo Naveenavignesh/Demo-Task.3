89d23caa2162a3d0c3e5f42d3812480fea3845c7
Naveenavignesh <naveenanavii4437@gmail.com>
2024-05-21 05:53:49 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//let obj1 = {name : "Person1" age:5}
+console.log("naveena")
+console.log("Person1")
+console.log("age:5")
+//}
+
+
+//let obj2= {age:5 "person1"  name}
+console.log("age:5")
+console.log("Person1")
+console.log("Priya")
+//}
